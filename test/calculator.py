05e94c7afd4f4a01eb300b778acf2dda68c0e5b1
codlxflare <148866504+codlxflare@users.noreply.github.com>
2024-06-12 14:55:30 +0300

    add app and test

---
+++ test/calculator.py
@@ -0,0 +1,16 @@
+def sum(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
+if __name__ == "__main__":
+    print("Calculator done")
